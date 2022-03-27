a3a55983576450187d71242fc2166efaced031f6
Michael Silva <michaelsilv@gmail.com>
2022-03-27 02:31:42 -0300

    Finalização da contagem da hora, inicio da inclusão da funcionalidade extra ( o cumprimento)

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,34 @@
-let d = new Date ();
-let h = d.getHours().toString().padStart(2,0); 
-let m = d.getMinutes();
-let s = d.getSeconds();
+function showtime(){
+    var d = new Date ();
+    var h = d.getHours().toString().padStart(2,0); // mostra a hora formatada
+    var m = d.getMinutes().toString().padStart(2,0); 
+    var s = d.getSeconds().toString().padStart(2,0); 
 
-let tempo = h + ':' + m + ':' + s;
+    var tempo = h + '' + ':' + '' + m + '' + ':' + s;
+    document.getElementById('relogio').innerHTML = tempo;
+    
+}
 
-setInterval(1000);
-console.log(tempo);
+showtime();
+let time = setInterval(showtime,1000);// função + tempo em milisegundos
+
+
+function cumprimentar (){
+   let d2= new Date ();
+   let h2 = d2.getHours(); // mostra a hora formatada
+   
+        if (h2 == 00 || h2 < 12) {
+            
+            var br = document.createElement("br");
+            var foo = document.getElementById("espaco");
+            foo.appendChild(br)
+            document.getElementById("cumprimento").innerHTML= 'Bom dia !' ;
+            
+            console.log('dia');
+            
+            
+           
+        } 
+}
+
+cumprimentar();
